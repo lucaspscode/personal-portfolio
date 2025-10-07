ce31f246d6d226d2d0d83cec25686aa96c76907b
fxlucas <dos-passos@outlook.com>
2025-10-07 12:48:22 -0300

    Mudanças na estrutura de projetos.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,7 @@ import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon4 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -45,11 +46,11 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="" /></a>
+              <a href="https://w.app/wxqich"><img src={navIcon4} alt="" /></a>
               <a href="#"><img src={navIcon2} alt="" /></a>
               <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
-            <a href="https://www.linkedin.com/in/dos-passos/"><button className="vvd" onClick={() => console.log('connect')} ><span>Let's Connect</span></button></a>
+            <a href="https://www.linkedin.com/in/dos-passos/"><button className="vvd" onClick={() => console.log('connect')} ><span><img src={navIcon1} alt="" />Let's Connect</span></button></a>
           </span>
         </Navbar.Collapse>
       </Container>
